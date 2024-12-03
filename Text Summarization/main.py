86afa8c4b15bd1e71baf39fb4de3fa4153772005
HHARSANI <61774014+HARSHHARSANI@users.noreply.github.com>
2024-12-03 09:19:35 +0530

    going back to previous stage

--- Text Summarization/main.py
+++ Text Summarization/main.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 mydb = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="manager",
+    password="12345678",
     database="news_db"  # Specify the database here
 )
 
